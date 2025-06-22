3b07d68b31b2514e0ec15f38b1d9882f2eacbf96
krishnan R <130322202+krish-2305@users.noreply.github.com>
2025-06-22 16:08:59 +0530

    Update gst_extractor.py

--- gst_verification/gst_extractor.py
+++ gst_verification/gst_extractor.py
@@ -6,8 +6,10 @@ from google import generativeai as genai
 import streamlit as st
 from dotenv import load_dotenv
 
-load_dotenv()
+
 api_key = os.getenv("GOOGLE_API_KEY")
+assert api_key is not None, "GOOGLE_API_KEY is not set in Railway!"
+
 genai.configure(api_key=api_key)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
